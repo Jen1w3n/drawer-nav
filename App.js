98ba9f5516952ed18be3003eb7f84dec8c5d839a
Jenna <112132967+Jen1w3n@users.noreply.github.com>
2024-01-26 09:17:19 +0200

    Added, configured and used bottom tabs. Open drawer button doesn't seem to work anymore.

--- App.js
+++ App.js
@@ -1,44 +1,44 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
 
-const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
+      <BottomTab.Navigator
         screenOptions={{
           headerStyle: { backgroundColor: "#604c8f" },
           headerTintColor: "white",
-          drawerActiveBackgroundColor: "#ebc5d3",
-          drawerActiveTintColor: "#ca849f",
-          drawerStyle: { backgroundColor: "#ccc" },
+          // drawerActiveBackgroundColor: "#ebc5d3",
+          // drawerActiveTintColor: "#ca849f",
+          // drawerStyle: { backgroundColor: "#ccc" },
+          tabBarActiveTintColor: "#604c8f",
         }}
       >
-        <Drawer.Screen
+        <BottomTab.Screen
           name="Welcome"
           component={WelcomeScreen}
           options={{
-            drawerLabel: "Welcome Screen",
-            drawerIcon: ({ color }) => (
-              <Ionicons name="home" color={color} size={18} />
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
             ),
           }}
         />
-        <Drawer.Screen
+        <BottomTab.Screen
           name="User"
           component={UserScreen}
           options={{
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name="person" color={color} size={size} />
             ),
           }}
         />
-      </Drawer.Navigator>
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
